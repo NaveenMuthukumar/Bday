c8d0f36e56a1ab0f5887606f32014e5ca21923d1
Naveen <naveenram2116@gmail.com>
2026-07-18 00:01:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -114,42 +114,7 @@ function createHeart() {
 setInterval(createHeart, 500);
 
 // Image Popup
-const images = document.querySelectorAll(".photos img");
 
-images.forEach(img => {
-
-    img.addEventListener("click", () => {
-
-        const overlay = document.createElement("div");
-
-        overlay.style.position = "fixed";
-        overlay.style.top = 0;
-        overlay.style.left = 0;
-        overlay.style.width = "100%";
-        overlay.style.height = "100%";
-        overlay.style.background = "rgba(0,0,0,.9)";
-        overlay.style.display = "flex";
-        overlay.style.justifyContent = "center";
-        overlay.style.alignItems = "center";
-        overlay.style.zIndex = "99999";
-
-        const photo = document.createElement("img");
-
-        photo.src = img.src;
-        photo.style.maxWidth = "90%";
-        photo.style.maxHeight = "90%";
-        photo.style.borderRadius = "20px";
-        photo.style.boxShadow = "0 0 40px white";
-
-        overlay.appendChild(photo);
-
-        overlay.onclick = () => overlay.remove();
-
-        document.body.appendChild(overlay);
-
-    });
-
-});
 
 // Auto Confetti Every 15 Seconds
 setInterval(() => {
