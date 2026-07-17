44d5333d184b115e18c8c1306bc29a9d1a25e2f5
Naveen <naveenram2116@gmail.com>
2026-07-17 23:58:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ const images = document.querySelectorAll(".photos img");
 
 images.forEach(img => {
 
-    img.addEventListener("none", () => {
+    img.addEventListener("click", () => {
 
         const overlay = document.createElement("div");
 
@@ -143,7 +143,7 @@ images.forEach(img => {
 
         overlay.appendChild(photo);
 
-        
+        overlay.onclick = () => overlay.remove();
 
         document.body.appendChild(overlay);
 
